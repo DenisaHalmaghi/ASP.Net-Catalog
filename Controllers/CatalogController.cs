5729d28acc6de93f64027a03b47c43204fc70946
DenisaHalmaghi <denisa@graffino.com>
2021-11-16 11:32:47 +0200

    finalize

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,10 +20,27 @@ namespace mobile.Controllers
     // }
     // public ViewResult List() => View(repository.Catalogs);
 
-    // GET: Intrari1
+    // GET: Catalogs1
     public async Task<IActionResult> Index()
     {
       return View(await _context.Catalogs.ToListAsync());
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var catalog = await _context.Catalogs
+          .FirstOrDefaultAsync(m => m.id == id);
+      if (catalog == null)
+      {
+        return NotFound();
+      }
+
+      return View(catalog);
     }
 
     // GET: Catalog/Create
@@ -43,6 +61,91 @@ namespace mobile.Controllers
       }
       return View(catalog);
     }
+    // GET: Catalogs1/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var catalog = await _context.Catalogs.FindAsync(id);
+      if (catalog == null)
+      {
+        return NotFound();
+      }
+      return View(catalog);
+    }
+
+    // POST: Catalogs1/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("id,marca,nume,prenume,nota1,nota2")] Catalog catalog)
+    {
+      if (id != catalog.id)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(catalog);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!CatalogExists(catalog.id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(catalog);
+    }
+
+    // GET: Catalogs1/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var catalog = await _context.Catalogs
+          .FirstOrDefaultAsync(m => m.id == id);
+      if (catalog == null)
+      {
+        return NotFound();
+      }
+
+      return View(catalog);
+    }
+
+    // POST: Catalogs1/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+      var catalog = await _context.Catalogs.FindAsync(id);
+      _context.Catalogs.Remove(catalog);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    private bool CatalogExists(int id)
+    {
+      return _context.Catalogs.Any(e => e.id == id);
+    }
   }
+
 
 }
